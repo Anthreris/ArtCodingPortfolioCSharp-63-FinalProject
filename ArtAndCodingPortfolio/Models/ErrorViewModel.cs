c19216af77a1acd4a9fe7242c3e1f4b381e9c260
Garrett Chitwood <gman@acvwa.com>
2026-04-13 16:34:05 -0700

    Implemented gitignore, added various tabs website features like Art, Code, and Contact.

---
+++ ArtAndCodingPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ArtAndCodingPortfolio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
