d6bcad74d479f06252d7283d5da4649305852d62
Garrett Chitwood <gman@acvwa.com>
2026-06-26 18:34:01 -0700

    Attempted to fix Basenji Image by moving path to project wwwroot folder. This failed to create results.

--- ArtAndCodingPortfolio/Data/PortfolioDbContext.cs
+++ ArtAndCodingPortfolio/Data/PortfolioDbContext.cs
@@ -1,11 +1,12 @@
+using System.Data;
 using ArtAndCodingPortfolio.Models;
-using Microsoft.EntityFrameworkCore;
+using Dapper;
 
 namespace ArtAndCodingPortfolio.Data;
 
-public class PortfolioDbContext : DbContext
-{
-    public PortfolioDbContext(DbContextOptions<PortfolioDbContext> options) : base(options) { } 
-    public DbSet<ArtPiece> ArtPieces { get; set; }
-    public DbSet<CodeProjectModel> CodeProjects { get; set; }
-}
+// public class PortfolioDbContext : DbContext
+// {
+//     public PortfolioDbContext(DbContextOptions<PortfolioDbContext> options) : base(options) { } 
+//     public DbSet<ArtPiece> ArtPieces { get; set; }
+//     public DbSet<CodeProjectModel> CodeProjects { get; set; }
+// }
